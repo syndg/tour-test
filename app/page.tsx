e39d415db6005bc00638e4f25046b84294ca98fe
syndg <sinchandsgpt9@gmail.com>
2024-06-05 20:16:11 +0530

    📦 NEW: Changes to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import { serverGetLoggedInUser } from "@/actions/serverGetLoggedInUser";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const data = await serverGetLoggedInUser();
+  const user = data?.user;
+
+  console.log({ user });
+
   return (
     <main className="grid h-[100svh] place-content-center text-center">
       This is just a dummy page.{" "}
-      <Link href="/login" className="underline text-blue-500">
-        Login
+      <Link
+        href={user ? "/profile" : "/login"}
+        className="underline text-blue-500"
+      >
+        {user ? "Go to profile" : "Login"}
       </Link>
     </main>
   );
